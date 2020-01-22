10acafa599e42fd3ac8e957ecf523b383bc94a66
Senthamizh <senthamilselvan@arimaglobal.com>
2020-01-22 11:35:07 +0530

    axios add and json placeholder data fetch to display on home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,55 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Home = () => {
-    return (
-        <div className="container">
-            <h4 className="text-center">Home</h4>
-            <p>List groups are a flexible and powerful component for displaying a series of content. Modify and extend them to support just about any content within.</p>
-        </div>
-    );
+import Axios from 'axios';
+
+class Home extends Component {
+
+    state = {
+        posts: []
+    };
+
+    componentDidMount() {
+        let _this = this;
+        Axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(function(res){
+            _this.setState({
+                posts: res.data.slice(0, 10)
+            });
+        });
+    }
+
+    render(){
+
+        let { posts } = this.state;
+
+        const postList = posts.length ? (
+            posts.map(post => {
+                return (
+                    <div className="card shadow-sm my-3" key={post.id}>
+                        <div className="card-body">
+                            <h5 className="card-title">{post.title}</h5>
+                            <p className="card-text">{post.body}</p>
+                        </div>
+                    </div>
+                );
+            })
+        ) : (            
+            <div className="card shadow-sm my-3">
+                <div className="card-body">
+                <p className="card-text text-center">No Posts yet</p>
+                </div>
+            </div>
+        );
+
+        return (
+            <div className="container">
+                <h4 className="text-center">Home</h4>
+                {postList}
+            </div>
+        );
+    }
 }
+
+
 
 export default Home;
